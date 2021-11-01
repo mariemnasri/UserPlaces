511d4b9e28c6976175bcc5e7701b7949b885e8c3
mariemnasri <mariemnasri06@gmail.com>
2021-11-01 14:41:21 +0100

    ChangeAppName+add div//

---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const NewPlace = () => {
+  return <h2>New place!</h2>;
+};
+export default NewPlace;
---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import PlaceList from "../components/PlaceList";
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire state building",
+    description: "One of the most famous sky scrapers in the world ",
+    imageUrl:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Ffr.m.wikiversity.org%2Fwiki%2FFichier%3ANYC_Empire_State_Building.jpg&psig=AOvVaw1ntTqTf0f5nzSCtG-HGM-z&ust=1635682533383000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIj5sf6A8vMCFQAAAAAdAAAAABAD",
+    adrress: "20 W 34th St, New York, NY 10001, États-Unis",
+    creator: "u1",
+    location: {
+      lat: 40.7487331,
+      lng: -73.9794846,
+    },
+  },
+  {
+    id: "p2",
+    title: "Empire",
+    description: "One of the most famous sky scrapers in the world ",
+    imageUrl:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Ffr.m.wikiversity.org%2Fwiki%2FFichier%3ANYC_Empire_State_Building.jpg&psig=AOvVaw1ntTqTf0f5nzSCtG-HGM-z&ust=1635682533383000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIj5sf6A8vMCFQAAAAAdAAAAABAD",
+    adrress: "20 W 34th St, New York, NY 10001, États-Unis",
+    creator: "u2",
+    location: {
+      lat: 40.7487331,
+      lng: -73.9794846,
+    },
+  },
+];
+
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter((place) => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+export default UserPlaces;
